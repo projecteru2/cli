0d290344c77a907bb65d0374d3ebde336bd7fb16
tonic <tonicbupt@gmail.com>
2021-02-23 13:59:44 +0800

    enhance cli, supporting some filtering

--- cmd/pod/resource.go
+++ cmd/pod/resource.go
@@ -2,6 +2,9 @@ package pod
 
 import (
 	"context"
+	"regexp"
+	"strconv"
+	"strings"
 
 	"github.com/juju/errors"
 	"github.com/projecteru2/cli/cmd/utils"
@@ -10,9 +13,89 @@ import (
 	"github.com/urfave/cli/v2"
 )
 
+var re = regexp.MustCompile(`(?P<name>cpu|memory|storage|volume)\s*(?P<op>>|>=|<|<=|==)\s*(?P<value>\d+.?\d*%?)`)
+
+func match(s string) map[string]string {
+	rv := make(map[string]string)
+	founds := re.FindStringSubmatch(s)
+	for i, name := range re.SubexpNames() {
+		if i > 0 && i < len(founds) {
+			rv[name] = founds[i]
+		}
+	}
+	return rv
+}
+
+func op(op string, left, right float64) bool {
+	switch {
+	case op == ">":
+		return left > right
+	case op == ">=":
+		return left >= right
+	case op == "<":
+		return left < right
+	case op == "<=":
+		return left <= right
+	case op == "==":
+		return left == right
+	default:
+		return false
+	}
+}
+
+func attr(nr *corepb.NodeResource, name string) float64 {
+	switch {
+	case name == "cpu":
+		return nr.CpuPercent
+	case name == "memory":
+		return nr.MemoryPercent
+	case name == "storage":
+		return nr.StoragePercent
+	case name == "volume":
+		return nr.VolumePercent
+	default:
+		return 0
+	}
+}
+
 type resourcePodOptions struct {
 	client corepb.CoreRPCClient
 	name   string
+	expr   string
+}
+
+func (o *resourcePodOptions) filter(nrs []*corepb.NodeResource) ([]*corepb.NodeResource, error) {
+	filter := match(o.expr)
+	if len(filter) == 0 {
+		return nrs, nil
+	}
+
+	var (
+		value   = filter["value"]
+		percent bool
+	)
+	if strings.HasSuffix(value, "%") {
+		value = value[:len(value)-1]
+		percent = true
+	}
+
+	v, err := strconv.ParseFloat(value, 64)
+	if err != nil {
+		return nil, err
+	}
+	if percent {
+		v /= 100
+	}
+
+	rv := []*corepb.NodeResource{}
+	for _, nr := range nrs {
+		l := attr(nr, filter["name"])
+		if !op(filter["op"], l, v) {
+			continue
+		}
+		rv = append(rv, nr)
+	}
+	return rv, nil
 }
 
 func (o *resourcePodOptions) run(ctx context.Context) error {
@@ -23,7 +106,12 @@ func (o *resourcePodOptions) run(ctx context.Context) error {
 		return err
 	}
 
-	describe.NodeResources(resp.NodesResource...)
+	nrs, err := o.filter(resp.NodesResource)
+	if err != nil {
+		return err
+	}
+
+	describe.NodeResources(nrs...)
 	return nil
 }
 
@@ -41,6 +129,7 @@ func cmdPodResource(c *cli.Context) error {
 	o := &resourcePodOptions{
 		client: client,
 		name:   name,
+		expr:   c.String("filter"),
 	}
 	return o.run(c.Context)
 }
