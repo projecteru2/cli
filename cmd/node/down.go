47992bf29d9df3c27c4cb92e78b5039da8fb430f
DuodenumL <qq2410088750@live.com>
2021-12-22 12:54:58 +0800

    skip setting "available: false" in "node down" (#81)

--- cmd/node/down.go
+++ cmd/node/down.go
@@ -37,7 +37,6 @@ func (o *setNodeDownOptions) run(ctx context.Context) error {
 	if do {
 		_, err := o.client.SetNode(ctx, &corepb.SetNodeOptions{
 			Nodename:  o.name,
-			StatusOpt: corepb.TriOpt_FALSE,
 			BypassOpt: corepb.TriOpt_TRUE,
 		})
 		if err != nil {
